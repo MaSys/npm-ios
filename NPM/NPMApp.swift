c58b900a8321a0e3ff72d50343eb2c0d4f61d262
Yaser Almasri <yaser@masys.mx>
2025-08-13 21:57:52 -0600

    Initial Commit

---
+++ NPM/NPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPMApp.swift
+//  NPM
+//
+//  Created by Yaser Almasri on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
